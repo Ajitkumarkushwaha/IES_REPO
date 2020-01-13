53751566e0a0501130cde788a4ec4389f0b5f6df
Ajitkumarkushwaha <naa.ajitkumar@gmail.com>
2020-01-13 20:25:31 +0530

    Test Java Class Is modified With Demo Class With Show Method

--- Test.java
+++ Test.java
@@ -1,7 +1,15 @@
+class Demo
+{
+	void show(){
+		System.out.println("-----From Show Method--------");
+	}
+}
 class Test
 {
 	public static void main(String[] args){
 			System.out.println("Hello...");
 			System.out.println("Welcome To GitHub");
+			Demo d = new Demo();
+			d.show();
 	}
 }
